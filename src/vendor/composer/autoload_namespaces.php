199253b2aaee3e020715a6606e0a3e5d80403f11
ajither <ajitheredacheril@hotmail.com>
2017-09-15 16:29:01 +0530

    composer updates

--- src/vendor/composer/autoload_namespaces.php
+++ src/vendor/composer/autoload_namespaces.php
@@ -7,5 +7,8 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'sendotp\\' => array($vendorDir . '/sendotp/sendotp/src'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Pimple' => array($vendorDir . '/pimple/pimple/src'),
+    'Jenssegers\\Mongodb' => array($vendorDir . '/jenssegers/mongodb/src'),
+    'Jenssegers\\Eloquent' => array($vendorDir . '/jenssegers/mongodb/src'),
 );
